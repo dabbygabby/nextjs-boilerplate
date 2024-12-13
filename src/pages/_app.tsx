37237afcb6a5185a3cd2d2e9bf4839cd19501234
Saurabh Dabral <saurabhdabral61@gmail.com>
2024-12-13 21:21:40 +0530

    fixed tailwind css issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import { useState } from 'react'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
